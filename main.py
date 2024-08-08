7a8160abd28277959d7fd4022004bd25be5317e8
Mohini-1998 <mohinimahalle18@gmail.com>
2024-08-08 12:41:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from color_reference_manual import print_reference_manual
-from get_numbers_from_color import get_pair_number_from_color
+MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
+MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
-if __name__ == "__main__":
-    print_reference_manual()
-    print(get_pair_number_from_color("Red","Blue"))
-    print(get_pair_number_from_color("White","Blue"))
+def print_colour_code_table():
+    table=create_colour_code_table()
+    for row in table:
+        print('--------------------------------------------')
+        print('| {:^11} | {:^12} | {:^12} |'.format(*row))
+    
+def create_colour_code_table():
+    table=[['Pair number','Major colour','Minor colour']]
+    pair_number=1
+    row=[]
+    for major_colour in MAJOR_COLORS:
+        for minor_colour in MINOR_COLORS:
+            row.append(pair_number)
+            row.append(major_colour)
+            row.append(minor_colour)
+            table.append(row)
+            pair_number+=1
+            row=[]
