04e95e2c7e03428022d684e82ce2ff4066482b55
Mohini-1998 <mohinimahalle18@gmail.com>
2024-08-08 12:40:41 +0530

    Update get_number_from_color_test.py

--- get_number_from_color_test.py
+++ get_number_from_color_test.py
@@ -1,10 +1,21 @@
-from get_number_from_color import get_pair_number_from_color
+MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
+MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
-def test_pair_to_number(major_color, minor_color, expected_pair_number):
-  pair_number = get_pair_number_from_color(major_color, minor_color)
-  assert(pair_number == expected_pair_number)
-
-if __name__ == '__main__':
-  test_pair_to_number('Black', 'Orange', 12)
-  test_pair_to_number('Violet', 'Slate', 25)
-  test_pair_to_number('Red', 'Orange', 7)
+def print_colour_code_table():
+    table=create_colour_code_table()
+    for row in table:
+        print('--------------------------------------------')
+        print('| {:^11} | {:^12} | {:^12} |'.format(*row))
+    
+def create_colour_code_table():
+    table=[['Pair number','Major colour','Minor colour']]
+    pair_number=1
+    row=[]
+    for major_colour in MAJOR_COLORS:
+        for minor_colour in MINOR_COLORS:
+            row.append(pair_number)
+            row.append(major_colour)
+            row.append(minor_colour)
+            table.append(row)
+            pair_number+=1
+            row=[]
